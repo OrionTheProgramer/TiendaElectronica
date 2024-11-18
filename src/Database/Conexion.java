ab40b58e18bb7e48615bee964c7ed106c228a5b8
Orion <oriongames360@gmail.com>
2024-11-18 18:32:52 -0300

    Primera version de apoyo

---
+++ src/Database/Conexion.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/chinoburger"; 
+            String user = "root";
+            String password = "OrionGames2029";
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexión establecida.");
+        } catch (SQLException e) {
+            System.out.println("Error de conexión: " + e.getMessage());
+        }
+        return conn;
+    }
+}
