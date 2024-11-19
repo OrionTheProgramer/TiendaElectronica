ed96d0319358668f2c424bdc8bdf03e0547dbb4f
Orion <oriongames360@gmail.com>
2024-11-18 21:07:16 -0300

    Creadas las clases cliente, clienteCLT, las vistas Principal, Inicio_sesin y registro.
    Cambio entre paneles incompleto y aun en prueba, el metodo es 
    CambiarPanel() y esta alojada en la vista Principal. Por si sola 
    funciona pero cuando se llama desde otra vista presenta problemas.

--- src/Controlador/ClienteCTL.java
+++ src/Controlador/ClienteCTL.java
@@ -4,10 +4,36 @@
  */
 package Controlador;
 
+import Database.Conexion;
+import Modelo.Cliente;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author orion
  */
 public class ClienteCTL {
-    
+    public static boolean registrarCliente(Cliente cliente) {
+        String query = "INSERT INTO cliente (nombre, correo, password) VALUES (?, ?, ?)";
+        boolean registroExitoso = false;
+
+        try (Connection conn = Conexion.connect();
+             PreparedStatement cliente_datos = conn.prepareStatement(query)) {
+
+            cliente_datos.setString(1, cliente.getNombre());
+            cliente_datos.setString(2, cliente.getCorreo());
+            cliente_datos.setString(3, cliente.getContraseña());
+
+            int filasInsertadas = cliente_datos.executeUpdate();
+            if (filasInsertadas > 0) {
+                registroExitoso = true;
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al registrar cliente: " + e.getMessage());
+        }
+
+        return registroExitoso;
+    }
 }
--- src/Modelo/Cliente.java
+++ src/Modelo/Cliente.java
@@ -9,5 +9,45 @@ package Modelo;
  * @author orion
  */
 public class Cliente {
+    private String nombre, correo, contraseña;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, String correo, String contraseña) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", correo=" + correo + ", contrase\u00f1a=" + contraseña + '}';
+    }
+    
     
 }
